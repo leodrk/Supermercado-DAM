7b9a875c3c81c23dba789b9f3da4b15f711cf7cb
leodrk <martos.marcelo@gmail.com>
2019-06-03 23:11:59 -0300

    agregado CajaService

---
+++ src/test/java/Supermercado/CajaServiceTest.java
@@ -0,0 +1,33 @@
+package Supermercado;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Supermercado.Service.CajaService;
+import Supermercado.model.Caja;
+import Supermercado.model.Producto;
+
+public class CajaServiceTest {
+
+	
+	private CajaService service;
+	private Caja caja;
+	
+	@Before
+	public void setUp() {
+		this.caja = new Caja();
+		this.service = new CajaService(caja);
+	}
+	
+	@Test
+	public void testRecuperarJugoDeNaranjaCodigo1101() {
+		Producto producto = service.registrarProducto(1101);
+		assertTrue(producto.getCodigo()==1101);
+		assertTrue(caja.getCosteTotalActual() == 50);
+		assertTrue(caja.getProductosACobrar().size() == 1);
+		
+	}
+	
+}
